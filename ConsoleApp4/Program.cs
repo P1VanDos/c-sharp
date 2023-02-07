86d997b10fd8f64d3e7584bd6a20606b49f62bc9
P1VanDos <123485933+P1VanDos@users.noreply.github.com>
2023-02-07 23:52:46 +0300

    add c#

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace DemoSplitFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1 zadanie
+            /*int[] omas = new int[12] { 5, -2, 3, -6, 7, 9, -4, 8, 1, 2, 7, -3 };
+            foreach (int elem in omas)
+            {
+                Console.Write($"{elem}\t");
+            }*/
+
+            //2 zadanie
+
+
+            /*int[] omas = new int[12] { 5, -2, 3, -6, 7, 9, -4, 8, 1, 2, 7, -3 };
+            foreach (int elem in omas)
+            {
+                if (elem > 0)
+                    Console.Write($"{elem}\t");
+            }*/
+
+            // 3 zadanie
+
+            /*int[] omas = new int[] { 18,-4,16,5,10,-7 };
+            foreach (int elem in omas)
+            {
+                Console.Write($"{elem}\t");
+            }
+*/
+
+            // 4 zadanie
+            Random rnd = new Random();
+            int n = rnd.Next(163,180);
+            int[] myArray = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                myArray[i] = rnd.Next(180);
+            }
+            foreach (int elem in myArray)
+            {
+                Console.WriteLine("{0} ", elem);
+
+            }
+        }
+    }
+}
